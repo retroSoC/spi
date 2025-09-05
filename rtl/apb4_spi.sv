c47dd41799e1aeda78d5c11a1ac9b5de25bf220b
Zihao Yu <yuzihao@ict.ac.cn>
2025-09-05 21:38:11 +0800

    fix(rtl): do not use interface for verilator and iverilog

--- rtl/apb4_spi.sv
+++ rtl/apb4_spi.sv
@@ -13,40 +13,65 @@
 module apb4_spi #(
     parameter int FIFO_DEPTH = 32
 ) (
+`ifdef __VERILOG__
+    `apb4_slave_if(apb4),
+    output                    spi_spi_sck_o,
+    output [`SPI_NSS_NUM-1:0] spi_spi_nss_o,
+    output [             3:0] spi_spi_io_en_o,
+    input  [             3:0] spi_spi_io_in_i,
+    output [             3:0] spi_spi_io_out_o,
+    output                    spi_irq_o
+`else
     apb4_if.slave apb4,
     spi_if.dut    spi
+`endif
 );
+
+`ifndef __VERILOG__
+  `apb4_slave_if2wire(apb4, apb4);
+  wire                    spi_spi_sck_o;
+  wire [`SPI_NSS_NUM-1:0] spi_spi_nss_o;
+  wire [             3:0] spi_spi_io_en_o;
+  wire [             3:0] spi_spi_io_in_i = spi.spi_io_in_i;
+  wire [             3:0] spi_spi_io_out_o;
+  wire                    spi_irq_o;
+  assign spi.spi_sck_o    = spi_spi_sck_o;
+  assign spi.spi_nss_o    = spi_spi_nss_o;
+  assign spi.spi_io_en_o  = spi_spi_io_en_o;
+  assign spi.spi_io_out_o = spi_spi_io_out_o;
+  assign spi.irq_o        = spi_irq_o;
+`endif
 
   spi_core #(
       .BUFFER_DEPTH(FIFO_DEPTH)
   ) u_spi_core (
-      .HCLK    (apb4.pclk),
-      .HRESETn (apb4.presetn),
-      .PADDR   (apb4.paddr[11:0]),
-      .PWDATA  (apb4.pwdata),
-      .PWRITE  (apb4.pwrite),
-      .PSEL    (apb4.psel),
-      .PENABLE (apb4.penable),
-      .PRDATA  (apb4.prdata),
-      .PREADY  (apb4.pready),
-      .PSLVERR (apb4.pslverr),
-      .events_o(spi.irq_o),
-      .spi_clk (spi.spi_sck_o),
-      .spi_csn0(spi.spi_nss_o[0]),
-      .spi_csn1(spi.spi_nss_o[1]),
-      .spi_csn2(spi.spi_nss_o[2]),
-      .spi_csn3(spi.spi_nss_o[3]),
-      .spi_sdo0(spi.spi_io_out_o[0]),
-      .spi_sdo1(spi.spi_io_out_o[1]),
-      .spi_sdo2(spi.spi_io_out_o[2]),
-      .spi_sdo3(spi.spi_io_out_o[3]),
-      .spi_oe0 (spi.spi_io_en_o[0]),
-      .spi_oe1 (spi.spi_io_en_o[1]),
-      .spi_oe2 (spi.spi_io_en_o[2]),
-      .spi_oe3 (spi.spi_io_en_o[3]),
-      .spi_sdi0(spi.spi_io_in_i[0]),
-      .spi_sdi1(spi.spi_io_in_i[1]),
-      .spi_sdi2(spi.spi_io_in_i[2]),
-      .spi_sdi3(spi.spi_io_in_i[3])
+      .HCLK    (apb4_pclk),
+      .HRESETn (apb4_presetn),
+      .PADDR   (apb4_paddr[11:0]),
+      .PWDATA  (apb4_pwdata),
+      .PWRITE  (apb4_pwrite),
+      .PSEL    (apb4_psel),
+      .PENABLE (apb4_penable),
+      .PRDATA  (apb4_prdata),
+      .PREADY  (apb4_pready),
+      .PSLVERR (apb4_pslverr),
+      .events_o(spi_irq_o),
+      .spi_clk (spi_spi_sck_o),
+      .spi_csn0(spi_spi_nss_o[0]),
+      .spi_csn1(spi_spi_nss_o[1]),
+      .spi_csn2(spi_spi_nss_o[2]),
+      .spi_csn3(spi_spi_nss_o[3]),
+      .spi_sdo0(spi_spi_io_out_o[0]),
+      .spi_sdo1(spi_spi_io_out_o[1]),
+      .spi_sdo2(spi_spi_io_out_o[2]),
+      .spi_sdo3(spi_spi_io_out_o[3]),
+      .spi_oe0 (spi_spi_io_en_o[0]),
+      .spi_oe1 (spi_spi_io_en_o[1]),
+      .spi_oe2 (spi_spi_io_en_o[2]),
+      .spi_oe3 (spi_spi_io_en_o[3]),
+      .spi_sdi0(spi_spi_io_in_i[0]),
+      .spi_sdi1(spi_spi_io_in_i[1]),
+      .spi_sdi2(spi_spi_io_in_i[2]),
+      .spi_sdi3(spi_spi_io_in_i[3])
   );
 endmodule
